56f70ecd5fb8b68b27592fa0267ee5f9a9f4a666
Tarun Puniya <tarunpuniya287@gmail.com>
2026-07-20 21:00:54 +0530

    Initialize

---
+++ fast api/day2.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import json
+app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+        return data
+    
+
+@app.get('/')
+def hello():
+    return{'message':"Patients Management System API"}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
+
