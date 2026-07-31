8dc276de62666e2add5ebff99e2351180c092035
Tarun Puniya <tarunpuniya287@gmail.com>
2026-07-31 21:00:35 +0530

    Learn about the path parameters and query parameters in fast api

---
+++ fast api/day3.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI , Path , HTTPException , Query
+import json
+app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+        return data
+    
+
+@app.get('/')
+def hello():
+    return{'message':"Patients Management System API"}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description="Id of the patient in db",example='P001')):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404,detail="Patient not found")
+
+
+@app.get('/sort')
+def sort_patients(sort_by: str=Query(...,description='Sort on the basis of height,weight or bmi '),order:str=Query('asc',description='sort in asc or dsc order')):
+    valid_fields=['height','weight','bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f'Invalid field select from{valid_fields}')
+
+    if order not in['asc','dsc']:
+        raise HTTPException(status_code=400,detail='Invalid order select between asc or dsc')
+
+    data = load_data()
+
+    sort_order=True if order=='dsc' else False
+
+    sorted_data = sorted(data.values(),key=lambda x: x.get(sort_by,0),reverse=sort_order)
+    
